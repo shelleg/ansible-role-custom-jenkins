2092c62407df2076ec621e7347f8b54af64c4c3c
Ivan Prokhorov <ivan.prokhorov@gmail.com>
2017-01-09 23:57:15 +0200

    Add nodes (slaves)

--- templates/config-proxy.groovy
+++ templates/config-proxy.groovy
@@ -4,7 +4,7 @@ def instance = Jenkins.getInstance()
 
 final String name = "{{ jenkins_proxy_host }}"
 final int port = {{ jenkins_proxy_port }}
-{% if jenkins_proxy_login %}
+{% if jenkins_proxy_host %}
 final String userName = "{{ jenkins_proxy_login | d('bogusUser' }}"
 final String password = "{{ jenkins_proxy_password | d('bogusPassword'}}"
 {% endif %}
